6eb9e56d7eef8ecfa46d40a607285ff18fb3f330
Steven Wilson <s.wilson@gordian.com>
2021-11-17 12:13:53 +0000

    Solution ready to add ef event store

---
+++ Areas/ToDo/Controllers/ToDoController.cs
@@ -0,0 +1,23 @@
+﻿using PracticalMicroservices.Areas.ToDo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PracticalMicroservices.Areas.ToDo.Server
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        public static List<TodoItem> Items { get; set; } = new List<TodoItem> { new TodoItem { Title = "Take out trash", IsDone = false } };
+
+        [HttpGet]
+        public ActionResult Get() => Ok(Items);
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            Items.Add(item);
+            return Ok();
+        }
+    }
+}
